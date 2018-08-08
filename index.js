f1a0d4b39dc02c35334825cd3b1f83a634983268
Marios Ioannidis <ioanmar80@gmail.com>
2018-08-08 16:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("Titanic").addEventListener("click", replaceTitle);
+  function replaceTitle() {
+    let castHtml = document.getElementById("cast");
+    
+    console.log("clicked");
+    document.getElementById("title").innerHTML = movies["Titanic"]["title"];
+    
+    if (castHtml == '<div id="cast"></div>') {
+      let cast = movies["Titanic"]["cast"];
+    var i;
+    for (i=0; i < cast.length; i++) {
+      document.getElementById("cast").innerHTML += "<p>Actor: " + cast[i]["actor"] + "</p>";
+      document.getElementById("cast").innerHTML += "<p>Role: " + cast[i]["role"] + "</p>";
+    }
+    }
+    
+  }
+  });
